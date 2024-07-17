c391d9a038c22995b2f003eff76ced8beef0d207
Alec Gibson <12036746+alecgibson@users.noreply.github.com>
2024-07-17 12:21:27 +0100

    🏷️ Fix types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,3 @@
-declare module '@reedsy/json0' {
-  import * as sharedb from 'sharedb';
-  export type Type = (typeof sharedb)['types']['map'][string];
-  export const type: Type;
-}
+import * as sharedb from 'sharedb';
+type Type = (typeof sharedb)['types']['map'][string];
+export const type: Type;
